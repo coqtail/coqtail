734bc857640bf9c180ce686ec9c43a0272effac7
gallais <gallais@7194d10d-a3ed-4628-aea2-7b4a0dfde000>
2010-02-08 22:50:26 +0000

    Début de site internet pour coqtail
    git-svn-id: svn+ssh://svn.code.sf.net/p/coqtail/code@21 7194d10d-a3ed-4628-aea2-7b4a0dfde000

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Coqtail - official website</title>
+</head>
+
+<frameset rows="0,*" border=0>
+	<frame name="void" src="void.html" noresize frameborder="no" border=0>
+  	<frame name="main" src="main.php" noresize frameborder="no" border=0>
+<noframes>
+<body>
+</body>
+</noframes>
+</frameset>
+</html>
