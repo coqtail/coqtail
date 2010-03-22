0aad6b2c92d51a6bf5fe974531b976201e023bca
hferee <hferee@7194d10d-a3ed-4628-aea2-7b4a0dfde000>
2010-03-22 11:47:04 +0000

    REALLY valid xhtml. (tricky: the page check is main.php, which is valid, whereas index.php wasn't)  -> what is void.html for ?
    git-svn-id: svn+ssh://svn.code.sf.net/p/coqtail/code@51 7194d10d-a3ed-4628-aea2-7b4a0dfde000

--- www/index.php
+++ www/index.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Coqtail - official website</title>
 </head>
 
-<frameset rows="0,*" border=0>
-	<frame name="void" src="void.html" noresize frameborder="no" border=0>
-  	<frame name="main" src="main.php" noresize frameborder="no" border=0>
-<noframes>
-<body>
-</body>
-</noframes>
+<frameset rows="*" >
+  	<frame name="main" src="main.php"/>
 </frameset>
 </html>
