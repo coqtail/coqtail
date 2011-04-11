3e4373af5d6e09c653b7a3fa93906ab95fd085b3
gallais <gallais@7194d10d-a3ed-4628-aea2-7b4a0dfde000>
2011-04-11 23:33:02 +0000

    SF logo on website
    git-svn-id: svn+ssh://svn.code.sf.net/p/coqtail/code@232 7194d10d-a3ed-4628-aea2-7b4a0dfde000

--- www/foot.inc.php
+++ www/foot.inc.php
@@ -3,12 +3,12 @@
   <div id="footer">
     <?php
       if($lang=="fr") {
-        echo "Dernières modifications : juillet 2010";
+        echo "Dernières modifications : avril 2011";
       } else {
-        echo "last modified: July 2010";
+        echo "last modified: April 2011";
       }
     ?>
-    <p>
+    <p id="w3c">
        <a href="http://validator.w3.org/check?uri=referer"><img
           style="border:0;width:88px;height:31px"
       src="img/valid-xhtml10.png"
@@ -22,6 +22,9 @@
           alt="<?=$css?>"
           title="<?=$css?>"
           /></a>
+    </p>
+    <p id="sourceforge">
+    <a href="http://sourceforge.net/projects/coqtail"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=298939&amp;type=9" width="80" height="15" alt="Get Coqtail at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>
     </p>
   </div>
 
