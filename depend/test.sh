ddab0342ad9546636102688537c86e4b4bd51757
gallais <gallais@7194d10d-a3ed-4628-aea2-7b4a0dfde000>
2012-01-11 14:03:36 +0000

    Preparing the 2nd release. Should we try to fix Complex/ or delay it for a 2.1 or something equivalent?
    git-svn-id: svn+ssh://svn.code.sf.net/p/coqtail/code@366 7194d10d-a3ed-4628-aea2-7b4a0dfde000

--- depend/test.sh
+++
@@ -1,2 +0,0 @@
-./coqdep -R ~/svn/coqtail/src/ Coqtail -coqlib /usr/lib/coq/ -dumpgraph ~/svn/coqtail/src/Reals/Dequa_examples.v > test.dot; tred < test.dot > test2.dot; dot -Tpdf test2.dot -o test.pdf
-
